e3ae32d45958e00192bb19eb448bdae3712a7951
BowDown097 <btd6modsyeet@gmail.com>
2024-12-26 18:44:18 -0800

    Add playing and paused indicators to window title

--- src/ui/widgets/webengineplayer/webchannelinterface.cpp
+++ src/ui/widgets/webengineplayer/webchannelinterface.cpp
@@ -2,11 +2,22 @@
 #include "qttubeapplication.h"
 #include "ui/views/viewcontroller.h"
 #include "utils/osutils.h"
+#include <QMainWindow>
+#include <QRegularExpression>
 
 WebChannelInterface::WebChannelInterface(QObject* parent) : QObject(parent)
 {
     connect(&qtTubeApp->settings(), &SettingsStore::preferredQualityChanged, &qtTubeApp->settings(), &SettingsStore::save);
     connect(&qtTubeApp->settings(), &SettingsStore::preferredVolumeChanged, &qtTubeApp->settings(), &SettingsStore::save);
+}
+
+void setWindowTitleSuffix(const QString& suffix)
+{
+    if (QMainWindow* mainWindow = qobject_cast<QMainWindow*>(qApp->activeWindow()))
+    {
+        static QRegularExpression suffixRegex(R"( \[(Playing|Paused)\]$)");
+        mainWindow->setWindowTitle(mainWindow->windowTitle().remove(suffixRegex).append(' ').append(suffix));
+    }
 }
 
 void WebChannelInterface::emitProgressChanged(double progress, double previousProgress)
@@ -16,10 +27,21 @@ void WebChannelInterface::emitProgressChanged(double progress, double previousPr
 
 void WebChannelInterface::handleStateChange(PlayerState state)
 {
-    if (state == STATE_PLAYING)
+    switch (state)
+    {
+    case STATE_PLAYING:
         OSUtils::toggleIdleSleep(true);
-    else if (state == STATE_PAUSED || state == STATE_ENDED)
+        setWindowTitleSuffix("[Playing]");
+        break;
+    case STATE_PAUSED:
         OSUtils::toggleIdleSleep(false);
+        setWindowTitleSuffix("[Paused]");
+        break;
+    case STATE_ENDED:
+        OSUtils::toggleIdleSleep(false);
+        break;
+    default: break;
+    }
 }
 
 void WebChannelInterface::switchWatchViewVideo(const QString& videoId)
