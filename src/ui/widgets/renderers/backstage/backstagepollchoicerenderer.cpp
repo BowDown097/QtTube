4dd8d75d26f0d2212060d7849ac9a19ba7355994
BowDown097 <btd6modsyeet@gmail.com>
2025-01-16 19:58:40 -0800

    Fix poll choice layout

--- src/ui/widgets/renderers/backstage/backstagepollchoicerenderer.cpp
+++ src/ui/widgets/renderers/backstage/backstagepollchoicerenderer.cpp
@@ -2,29 +2,24 @@
 #include "innertube.h"
 #include <QBoxLayout>
 #include <QLabel>
-#include <QProgressBar>
 
 constexpr QLatin1String NotSelectedStylesheet("QProgressBar::chunk { background-color: rgba(255, 255, 255, 0.2) }");
 
 BackstagePollChoiceRenderer::BackstagePollChoiceRenderer(QWidget* parent)
-    : ClickableWidget<QWidget>(parent),
+    : ClickableWidget<QProgressBar>(parent),
       m_choiceTextLabel(new QLabel(this)),
-      m_percentageLabel(new QLabel(this)),
-      m_progressBar(new QProgressBar(this))
+      m_layout(new QHBoxLayout(this)),
+      m_percentageLabel(new QLabel(this))
 {
     setClickable(true);
-    m_choiceTextLabel->setMaximumWidth(width() - 100);
+    setMaximum(100);
+    setTextVisible(false);
 
-    m_progressBar->setFixedWidth(width());
-    m_progressBar->setMaximum(100);
-    m_progressBar->setTextVisible(false);
+    m_layout->addWidget(m_choiceTextLabel, 0, Qt::AlignLeft);
+    m_layout->addWidget(m_percentageLabel, 0, Qt::AlignRight);
+    m_layout->setContentsMargins(5, 0, 20, 0);
 
-    m_innerLayout = new QHBoxLayout(m_progressBar);
-    m_innerLayout->addWidget(m_choiceTextLabel, 0, Qt::AlignLeft);
-    m_innerLayout->addWidget(m_percentageLabel, 0, Qt::AlignRight);
-    m_innerLayout->setContentsMargins(5, 0, 20, 0);
-
-    connect(this, &ClickableWidget<QWidget>::clicked, this, &BackstagePollChoiceRenderer::choose);
+    connect(this, &ClickableWidget<QProgressBar>::clicked, this, &BackstagePollChoiceRenderer::choose);
 }
 
 void BackstagePollChoiceRenderer::choose()
@@ -36,13 +31,13 @@ void BackstagePollChoiceRenderer::choose()
 
 bool BackstagePollChoiceRenderer::hasStyle() const
 {
-    return !m_progressBar->styleSheet().isEmpty();
+    return !styleSheet().isEmpty();
 }
 
 void BackstagePollChoiceRenderer::reset()
 {
-    m_progressBar->reset();
-    m_progressBar->setStyleSheet(QString());
+    QProgressBar::reset();
+    setStyleSheet(QString());
     m_percentageLabel->clear();
 }
 
@@ -55,11 +50,6 @@ void BackstagePollChoiceRenderer::setData(const InnertubeObjects::PollChoice& po
 void BackstagePollChoiceRenderer::setValue(double value, bool selected)
 {
     m_percentageLabel->setText(selected ? m_data.votePercentageIfSelected : m_data.votePercentageIfNotSelected);
-    m_progressBar->setStyleSheet(selected ? QLatin1String() : NotSelectedStylesheet);
-    m_progressBar->setValue(value);
-}
-
-int BackstagePollChoiceRenderer::value() const
-{
-    return m_progressBar->value();
+    setStyleSheet(selected ? QLatin1String() : NotSelectedStylesheet);
+    QProgressBar::setValue(value);
 }
--- src/ui/widgets/renderers/backstage/backstagepollchoicerenderer.h
+++ src/ui/widgets/renderers/backstage/backstagepollchoicerenderer.h
@@ -1,12 +1,12 @@
 #pragma once
 #include "innertube/objects/backstage/poll/pollchoice.h"
 #include "ui/widgets/clickablewidget.h"
+#include <QProgressBar>
 
 class QHBoxLayout;
 class QLabel;
-class QProgressBar;
 
-class BackstagePollChoiceRenderer : public ClickableWidget<QWidget>
+class BackstagePollChoiceRenderer : public ClickableWidget<QProgressBar>
 {
     Q_OBJECT
 public:
@@ -14,18 +14,15 @@ public:
     bool hasStyle() const;
     void reset();
     void setData(const InnertubeObjects::PollChoice& pollChoice);
-
     void setValue(double value, bool selected);
-    int value() const;
 
     double voteRatioIfNotSelected() const { return m_data.voteRatioIfNotSelected; }
     double voteRatioIfSelected() const { return m_data.voteRatioIfSelected; }
 private:
     QLabel* m_choiceTextLabel;
     InnertubeObjects::PollChoice m_data;
-    QHBoxLayout* m_innerLayout;
+    QHBoxLayout* m_layout;
     QLabel* m_percentageLabel;
-    QProgressBar* m_progressBar;
 private slots:
     void choose();
 };
